5d66d85b01817eb736912a74b0a5cdb245167067
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-10 22:23:32 -0700

    Update to App.js to handle switching muliple components using react router dom

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -1,79 +1,24 @@
+import React, { Component } from 'react';
+import Stock from './Stock';
+import Home from './Home';
+import Search from './Search';
+import StockForm from './StockForm';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-
-import React, { useEffect, useState, useCallback } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-import axios from 'axios'; 
-// import ReactNotification, { store } from 'react-notifications-component'
-import Customers from './components/Stocks';
-
-const App= () => {
-  const [ stockList, setStockList ] = useState([]);
-  const [ errorMessage, setErrorMessage ] = useState(null);
-
-  const getStocks = useCallback(() => {
-    axios.get('http://localhost:8080/api/stocks')
-    .then((response) => {
-      setStockList(response.data);
-    })
-    .catch((error) => {
-      setErrorMessage(Object.values(error.response.data.errors));
-    });
-  }, []);
-
-  return (
-
-    <Router>
-      <nav className="navbar navbar-expand-lg navbar-light" id ="background-color">
-        <a href="#"><img src = "https://i.imgur.com/3CsrAvY.jpg" className= "App-logo" alt = "logo"/></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link to="/" className="App-nav-links__item font-color">Home</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link to="/stocks" className="App-nav-links__item font-color">Stocks</Link>
-            </li>
-
-            <li className="nav-item">
-              <Link to="/search" className="App-nav-links__item font-color">Stock Search</Link>
-            </li>
-
-          </ul>
-        </div>
-    </nav>
- 
-        {/* <section className="App-errors">
-          <ReactNotification />
-        </section> */}
-
-    <main className="App-content">
-      <Switch>
-      <Route path="/search">
-          {/* <Search results={searchResults} onSearchMovieCallback={searchMovies} addMovieCallback={addMovie} /> */}
-        </Route>
-        <Route path="/stock search">
-          {/* <Movies list={movieList} onSelectCallback={selectMovie} /> */}
-        </Route>
-        <Route path="/stocks">
-          {/* <Customers list={customerList} onSelectCallback={selectCustomer} /> */}
-        </Route>
-      </Switch>
-    </main>
-  </Router>
-  );
+class App extends Component {
+  state = {  }
+  render() { 
+    return (
+      <Router>
+        <Switch>
+          <Route path='/'exact={true} component={Home}/>
+          <Route path='/stocks' exact={true} component={Stock}/>
+          <Route path='/stock' exact={true} component={StockForm}/>
+          <Route path='/search' exact={true} component={Search}/>
+        </Switch>
+      </Router>
+     );
+  }
 }
-
+ 
 export default App;

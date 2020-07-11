32105cce2b32dae019113ee57b85cfe47977a1c7
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-11 10:11:46 -0700

    Updated the nav bar

--- stock-tracker/src/AppNav.js
+++ stock-tracker/src/AppNav.js
@@ -6,11 +6,12 @@ class AppNav extends Component {
     render() { 
         return ( 
             <div>
-                <Navbar color="dark" dark expand="md">
-                    <NavbarBrand href="/">Stock Tracker Application</NavbarBrand>
+                <Navbar light expand="md" id ="background-color">
+                    <a href="#"><img src = "https://i.imgur.com/3CsrAvY.jpg" className= "App-logo" alt = "logo"/></a>
+                    {/* <NavbarBrand href="/">Stock Tracker Application</NavbarBrand> */}
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                            <   NavLink href="/">Home</NavLink>
+                                <NavLink href="/" >Home</NavLink>
                             </NavItem>
                             <NavItem>
                                 <NavLink href="/stocks">Stocks</NavLink>

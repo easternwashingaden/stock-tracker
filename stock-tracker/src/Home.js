d7a83224b46314eceb77313098be0715c27d5b45
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-11 10:11:59 -0700

    Added slide images to the home page

--- stock-tracker/src/Home.js
+++ stock-tracker/src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AppNav from './AppNav';
+import 'bootstrap/dist/css/bootstrap.css';
 class Home extends Component {
     state = {  }
     render() { 
@@ -9,6 +10,19 @@ class Home extends Component {
                 <h1 style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
                     Welcome to Stock Tracker Application
                 </h1>
+                <div id="my-carousel" class="carousel slide" data-interval="3000" data-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img className="d-block w-100" src="https://i.imgur.com/BKHxTge.jpg" alt="First slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="https://i.imgur.com/fqfOIEE.jpg" alt="Second slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="https://i.imgur.com/nHYJANK.png" alt="Third slide"/>
+                        </div>
+                    </div>
+                </div>
             </div>
             
         );

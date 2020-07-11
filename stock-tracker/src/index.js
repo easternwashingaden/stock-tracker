02fd23148067de575136c6401968d8d306ff13ec
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-10 22:24:27 -0700

    Changed App to be rendered

--- stock-tracker/src/index.js
+++ stock-tracker/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Stock from './Stock'
+import Home from './Home'
 
 ReactDOM.render(
   <React.StrictMode>

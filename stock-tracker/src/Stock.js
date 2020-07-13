ab286782b995e2f39201ac90c8c588cd694930ba
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-13 11:29:58 -0700

    Added api call for the current prices

--- stock-tracker/src/Stock.js
+++ stock-tracker/src/Stock.js
@@ -4,50 +4,91 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {Table} from 'reactstrap';
 import Moment from 'react-moment';
 import { Button, Container } from 'reactstrap';
+import axios from 'axios';
 
 
  
 import "react-datepicker/dist/react-datepicker.css";
 
-// Each componant has two states - state and render function - stat is internal thing inside the component
-
 class Stock extends Component {
-    state = {
-        isLoading : true,
-        Stocks : []
+    constructor(props){
+        super(props);
+        this.state ={
+            message: "",
+            isLoading : true,
+            currentPrices : [],
+            results : [],
+            Stocks : []
+        }
     }
-    //sync: when you make a request, you have to wait for the response...
-    //async: good for user experience. you don't have to wait for the response...
+
+    async getStockArray(){
+        const res = await axios.get('/api/stocks');
+        this.setState({Stocks: res.data});
+        await this.loadPrices(res.data)   
+    }
+
     async componentDidMount(){
-        const response = await fetch('/api/stocks');
-        const body = await response.json();
-        this.setState({Stocks :body, isLoading: false});
+        await this.getStockArray();
     }
+
+    async loadPrices(tickers){
+        const API_KEY = '6D6O68MT7WRRNJOM';
+        axios.all(tickers.map(stock => axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stock.ticker}&outputsize=compact&apikey=${API_KEY}`)))
+            .then (axios.spread((...res) => {
+                console.log(res);
+                
+                const latestPrices = res.map(element =>{
+                    for (var key in element.data['Time Series (Daily)']) {
+                        const allPrices = element.data['Time Series (Daily)'][key]['4. close']; 
+                        console.log(allPrices)
+                        return allPrices
+        
+                    }  
+        
+                })
+                
+                this.setState({ currentPrices: latestPrices, isLoading : false })
+               
+            }))
+
+            .catch((error) =>{
+                console.log(error)
+            })
+            
+    }
+    
 
     async remove(id){
         await fetch(`/api/stocks/${id}`,{
-          method: 'DELETE',
-          headers: {
+            method: 'DELETE',
+            headers: {
             'Accept': 'applicaion/json',
             'Content-Type' : 'application/json'
-          }
+            }
         }).then(()=>{
-          let updatedStocks = [...this.state.Stocks].filter(i => i.id !== id);
-          this.setState({Stocks : updatedStocks});
+            let updatedStocks = [...this.state.Stocks].filter(i => i.id !== id);
+            this.setState({Stocks : updatedStocks});
         });
-      }
-
+    }
     
+
     render() { 
-        const {Stocks, isLoading} = this.state;
+        const {Stocks, currentPrices,isLoading} = this.state;
         if(isLoading)
             return(<div>Loading...</div>)
+        
         let rows = 
-        Stocks.map(stock =>
+        Stocks.map((stock, i) =>
             <tr key = {stock.id} >
             <td>{stock.id}</td>
             <td>{stock.ticker}</td>
             <td>{stock.share}</td>
+            <td>{stock.price}</td>
+            <td>{currentPrices[i]}</td>
+            <td>{currentPrices[i] - stock.price}</td>
+            <td>{stock.price}</td>
+            <td>{stock.price}</td>
             <td>{stock.price}</td>
             <td><Moment date = {stock.purchasedDate} format = "YYYY/MM/DD"/></td>
             <td><Button size= 'sm' color='danger' onClick={()=> this.remove(stock.id)}>Delete</Button></td>
@@ -67,6 +108,11 @@ class Stock extends Component {
                                 <th>Ticker</th>
                                 <th>Share</th>
                                 <th>Purchased Price</th>
+                                <th>Current Price</th>
+                                <th>Total Gain/Loss</th>
+                                <th>%Gain/Loss</th>
+                                <th>Total Equity</th>
+                                <th>Total Cost</th>
                                 <th>Purchased Date</th>
                                 <th>Action</th>
                             </tr>
@@ -74,6 +120,7 @@ class Stock extends Component {
                             <tbody>
                                 {rows}
                             </tbody>
+                            
 
                         </Table>
                     </Container>
@@ -84,5 +131,6 @@ class Stock extends Component {
         );
     }
 }
+
  
 export default Stock;

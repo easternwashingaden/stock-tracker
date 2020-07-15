1b7507f0b4a6598590041ee270d62825d0ac77f3
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-15 13:01:06 -0700

    Add another route to the nav bar

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -5,6 +5,7 @@ import StockPerformance from './StockPerformance';
 import StockForm from './StockForm';
 import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import Portfolio from './Portfolio';
+import Sale from './Sale';
 
 class App extends Component {
   state = {  }
@@ -16,6 +17,7 @@ class App extends Component {
           <Route path='/portfolio' exact={true} component={Portfolio}/>
           <Route path='/stocks' exact={true} component={Stock}/>
           <Route path='/stock' exact={true} component={StockForm}/>
+          <Route path='/history' exact={true} component={Sale}/>
           <Route path='/stockperformance' exact={true} component={StockPerformance}/>
         </Switch>
       </Router>

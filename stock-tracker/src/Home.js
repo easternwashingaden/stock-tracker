54daa067769bf98295447a8fde084810b7fdecf0
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-18 17:01:50 -0700

    Added slides show on the homepage

--- stock-tracker/src/Home.js
+++ stock-tracker/src/Home.js
@@ -1,29 +1,85 @@
 import React, { Component } from 'react';
 import AppNav from './AppNav';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import Carousel from 'react-bootstrap/Carousel';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+
 class Home extends Component {
     state = {  }
     render() { 
         return (
-            <div>
-                <AppNav/>
-                <h1 style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-                    Welcome to Stock Tracker Application
-                </h1>
-                <div id="my-carousel" class="carousel slide" data-interval="3000" data-ride="carousel">
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img className="d-block w-100" src="https://i.imgur.com/BKHxTge.jpg" alt="First slide"/>
+            <section>
+                <div>
+                    <AppNav/>
+                    
+                    <h1 style={{textAlign: 'center', padding : "4rem", background: "lightgray", fontFamily: "cursive", fontWeight: "bold"}}>
+                        Welcome to Stock Tracker Application   
+                    </h1>
+                    
+
+                    <div style = {{margin: "5rem"}} className = "container">
+                        <div className="row">
+                        <div className="col card">
+
+                            <h4 className="card-header info-color white-text text-center py-3" style = {{background: "lightseagreen", fontWeight: 'bold', fontFamily: 'cursive'}}>Log In</h4>
+                           
+                            <Form style = {{alignContent: "center", marginLeft: "8rem", marginTop: "2rem"}}>
+                                <Form.Group className = "col-md-7" controlId="formBasicEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" />
+                                    {/* <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                    </Form.Text> */}
+                                </Form.Group>
+
+                                <Form.Group className = "col-md-7" controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" />
+                                </Form.Group>
+                                
+                                <Button style = {{marginLeft: "1rem", marginBottom: "2rem"}} variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </Form>
+                            
                         </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="https://i.imgur.com/fqfOIEE.jpg" alt="Second slide"/>
+                        
+                        <div className = "reduce-content col">
+                            <Carousel style={{alignContent: 'center'}}>
+                                <Carousel.Item>
+                                    <img
+                                    className="image-fixed-size"
+                                    src="https://i.imgur.com/BKHxTge.jpg"
+                                    alt="First slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                    className="image-fixed-size"
+                                    src="https://i.imgur.com/vse4w7N.jpg"
+                                    alt="Third slide"
+                                    />
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                    className="image-fixed-size"
+                                    src="https://i.imgur.com/jAbUMQP.jpg"
+                                    alt="Third slide"
+                                    />
+
+                                <Carousel.Caption>
+                                </Carousel.Caption>
+                                </Carousel.Item>
+                                </Carousel>
                         </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="https://i.imgur.com/nHYJANK.png" alt="Third slide"/>
+                        
                         </div>
                     </div>
-                </div>
-            </div>
+                        
+                    </div>
+            </section>
             
         );
     }

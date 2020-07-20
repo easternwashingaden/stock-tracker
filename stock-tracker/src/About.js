520968ba3b4830e1e40620dcfc318fb60615a114
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-20 16:55:34 -0700

    Added about tab

--- stock-tracker/src/About.js
+++ stock-tracker/src/About.js
@@ -4,7 +4,11 @@ class About extends Component {
   state = {  }
   render() { 
     return (
-      
+      <section class="lak-bio">
+        <div class="bio"></div>
+        <h1>Biography</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dolor quisquam perferendis aliquam recusandae quaerat non ratione maxime corporis quo atque minus eum tenetur facere veniam assumenda, nisi sed esse repellendus vero dignissimos culpa at et voluptate? Commodi voluptas neque iure, sed ad sequi molestiae quasi quidem magni assumenda nulla illo perspiciatis soluta optio illum quaerat odio quisquam numquam facere consectetur dolor. Dicta officiis dignissimos error magnam eveniet natus reprehenderit repudiandae totam quos. Suscipit atque, facere tempore iste debitis rem non! Voluptatem aliquid porro quaerat consectetur ratione laboriosam quae, explicabo repellat odit minus soluta necessitatibus a? Aliquam fuga, nihil deleniti harum laudantium id ad iure assumenda modi quidem sed culpa quos fugiat numquam magnam soluta beatae hic possimus delectus ea adipisci nemo porro. Molestias sunt magnam enim possimus harum vitae eum nam repellat corrupti ratione, soluta rerum, sed odio eaque dicta veritatis! Fugiat ad libero earum a quaerat veniam quia, debitis voluptatibus corrupti culpa, qui fuga. Assumenda odit ipsum cumque suscipit cupiditate aperiam, eaque illum laudantium, eum aut sed. Harum ad rerum enim tempore vero? Cumque dolores dolore dignissimos iste, repellat voluptatibus quos repudiandae corrupti maiores quis. Consequatur obcaecati enim vel molestiae laborum a expedita, omnis harum porro error! Impedit.</p>
+      </section>
 
     );
   }

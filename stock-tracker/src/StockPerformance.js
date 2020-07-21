4b40fa649a8daa027daf6c876655f9d38699da5e
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-20 23:56:29 -0700

    Tried to get company information through another api call

--- stock-tracker/src/StockPerformance.js
+++ stock-tracker/src/StockPerformance.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 
 // Resource: https://www.youtube.com/watch?v=T26V1aSEtJE&t=1381s
 class StockPerformance extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -15,7 +16,18 @@ class StockPerformance extends React.Component {
       message: "",
       isLoading: false,
       stockChartXValues: [],
-      stockChartYValues: []
+      stockChartYValues: [],
+      companyInfo: {
+        symbol: '',
+        companyName: '',
+        industry: '',
+        website:'',
+        description: '',
+        CEO:'',
+        employees:'',
+        country: ''
+
+      }
     }
     this.cancel= "";
     this.handleSumbit = this.handleSumbit.bind(this);
@@ -74,6 +86,38 @@ class StockPerformance extends React.Component {
             });
           }
         });
+
+        // call another api
+
+        axios.get(`https://sandbox.iexapis.com/stable/stock/${query}/company?token=Tsk_6a8a871a130d4dc09f1e0403eebb0e0b`)
+        .then ((response) => {
+          console.log(response);
+          const companyProfile= {
+            symbol: response.symbol,
+            companyName: response.data.companyName,
+            industry: response.data.industry,
+            website:response.data.website,
+            description: response.data.description,
+            CEO:response.data.CEO,
+            employees:response.data.employees,
+            country: response.data.country
+          }
+
+          this.setState({companyInfo : companyProfile})
+
+          console.log(response.data)
+          
+        })
+        .catch((error) => {
+          if (axios.isCancel(error) || error) {
+            this.setState({
+              loading: false,
+              message: 'Failed to fetch results.Please check network',
+            });
+          }
+        });
+
+
         this.setState({
           query: ""
         });
@@ -86,7 +130,7 @@ class StockPerformance extends React.Component {
 
   
   render(){
-    const { query } = this.state
+    const { query, companyInfo } = this.state
     return (
       <section>
         <div>
@@ -110,6 +154,7 @@ class StockPerformance extends React.Component {
               </div>
             </form>
           </div>
+          <div> {companyInfo.symbol}</div>
         {this.state.stockChartXValues && this.state.stockChartYValues.length ? <Plot
           data={[
           {

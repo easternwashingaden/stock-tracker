6502f07883578db7821f15d9c3c881cc85af95b9
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-22 03:12:16 -0700

    Updated the homepage

--- stock-tracker/src/Home.js
+++ stock-tracker/src/Home.js
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Carousel from 'react-bootstrap/Carousel';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import LogInForm from './LogInForm'
 
 class Home extends Component {
     state = {  }
@@ -14,35 +15,38 @@ class Home extends Component {
                 <div>
                     <AppNav/>
                     
-                    <h1 style={{textAlign: 'center', padding : "4rem", background: "lightgray", fontFamily: "cursive", fontWeight: "bold"}}>
+                    {/* <h1 style={{textAlign: 'center', padding : "4rem", background: "lightgray", fontWeight: "bold"}}>
                         Welcome to Stock Tracker Application   
-                    </h1>
+                    </h1> */}
                     
 
                     <div style = {{margin: "5rem"}} className = "container">
                         <div className="row">
                         <div className="col card">
 
-                            <h4 className="card-header info-color white-text text-center py-3" style = {{background: "lightseagreen", fontWeight: 'bold', fontFamily: 'cursive'}}>Log In</h4>
+                            <h4 className="card-header info-color white-text text-center py-3" style = {{background: "lightseagreen", fontWeight: 'bold'}}>Log In</h4>
                            
-                            <Form style = {{alignContent: "center", marginLeft: "8rem", marginTop: "2rem"}}>
-                                <Form.Group className = "col-md-7" controlId="formBasicEmail">
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" />
+                            {/* <Form style = {{alignContent: "center", marginLeft: "8rem", marginTop: "2rem"}}> */}
+                                {/* <Form.Group className = "col-md-7" controlId="formBasicEmail"> */}
+                                    {/* <Form.Label>Email address</Form.Label> */}
+                                    {/* <Form.Control type="email" placeholder="Email" /> */}
                                     {/* <Form.Text className="text-muted">
                                     We'll never share your email with anyone else.
                                     </Form.Text> */}
-                                </Form.Group>
+                                {/* </Form.Group> */}
 
-                                <Form.Group className = "col-md-7" controlId="formBasicPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" />
-                                </Form.Group>
+                                {/* <Form.Group className = "col-md-7" controlId="formBasicPassword"> */}
+                                    {/* <Form.Label>Password</Form.Label> */}
+                                    {/* <Form.Control type="password" placeholder="Password" /> */}
+                                {/* </Form.Group> */}
                                 
-                                <Button style = {{marginLeft: "1rem", marginBottom: "2rem"}} variant="primary" type="submit">
-                                    Submit
-                                </Button>
-                            </Form>
+                                {/* <Button style = {{marginLeft: "1rem", marginBottom: "2rem"}} variant="primary" type="submit"> */}
+                                    {/* Submit */}
+                                {/* </Button> */}
+                            {/* </Form> */}
+                            <div style = {{alignContent: "center", marginLeft: "8rem", marginTop: "2rem"}}>
+                                <LogInForm/>
+                            </div>
                             
                         </div>
                         

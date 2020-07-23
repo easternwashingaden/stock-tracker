0b395aa9dd499d2e96827dec29a9323f5b0efe4e
tithvorlakmok <tithvorlakmok@gmail.com>
2020-07-23 03:25:10 -0700

    Update the field validation form

--- stock-tracker/src/LogInForm.js
+++ stock-tracker/src/LogInForm.js
@@ -50,36 +50,38 @@ class LogInForm extends Component {
     }
 
     if (typeof fields["username"] !== "undefined") {
-      if (!fields["username"].match(/^[a-zA-Z ]*$/)) {
+      if (fields["username"] !== "Somany Mok") {
         formIsValid = false;
-        errors["username"] = "*Please enter alphabet characters only.";
+        errors["username"] = "*Wrong user name please try again.";
       }
     }
 
-    if (!fields["emailid"]) {
-      formIsValid = false;
-      errors["emailid"] = "*Please enter your email-ID.";
-    }
+    // if (!fields["emailid"]) {
+    //   formIsValid = false;
+    //   errors["emailid"] = "*Please enter your email-ID.";
+    // }
 
-    if (typeof fields["emailid"] !== "undefined") {
-      //regular expression for email validation
-      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-      if (!pattern.test(fields["emailid"])) {
-        formIsValid = false;
-        errors["emailid"] = "*Please enter valid email-ID.";
-      }
-    }
+    // if (typeof fields["emailid"] !== "undefined") {
+    //   //regular expression for email validation
+    //   var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    //   if (!pattern.test(fields["emailid"])) {
+    //     formIsValid = false;
+    //     errors["emailid"] = "*Please enter valid email-ID.";
+    //   }
+    // }
     if (!fields["password"]) {
       formIsValid = false;
       errors["password"] = "*Please enter your password.";
     }
 
     if (typeof fields["password"] !== "undefined") {
-      if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
-        formIsValid = false;
+      // if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+      if (fields["password"] !== "Loveada!"){
+      formIsValid = false;
         errors["password"] = "*Please enter secure and strong password.";
       }
     }
+  
 
     this.setState({
       errors: errors
@@ -98,11 +100,11 @@ class LogInForm extends Component {
           <div className="errorMsg">{this.state.errors.username}</div>
         </FormGroup>
 
-        <FormGroup>
+        {/* <FormGroup>
           <label>Email</label>
           <input type="text" name="emailid" value={this.state.fields.emailid} onChange={this.handleChange}  />
           <div className="errorMsg">{this.state.errors.emailid}</div>
-        </FormGroup>
+        </FormGroup> */}
 
         <FormGroup>
           <label>Password</label>
